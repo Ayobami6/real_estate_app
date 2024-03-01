f245d99717b0c755e4736db7d2512a5f580b3e36
ayobamialaran <alaran@riffutures.com>
2024-03-01 15:24:16 +0100

    django initial startproject boiler codes

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from  django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    @staticmethod
+    def email_validator(email):
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValidationError({"email": _("Please enter a valid email address.")})
+
+
